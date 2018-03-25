b6ce45c28027e96d1302b6684cb6be89046f9d35
elwin <elwin.hsiao@gmail.com>
2018-03-25 23:09:53 +0800

    init add

---
+++ game.js
@@ -0,0 +1,9 @@
+import './weapp-adapter'
+import './resource/js/three/three.js'
+import './resource/js/three/stats.min.js'
+import './resource/js/three/firstPersonControl.js'
+import './resource/js/three/helvetiker_regular.typeface.js'
+
+import './solarsystem.js'
+
+
